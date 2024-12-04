9ab936528f3d8653668d904ee4c5f0db4d376806
MicaCaceres <mica.caceres@live.com>
2024-12-04 15:08:33 -0300

    acualice requirements

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import NavBar from "./components/NavBar/Nav.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Reservas from "./components/Reservas/Reservas";
 import Canchas from "./components/Canchas/Canchas";
 import Home from "./utils/Home";
@@ -12,6 +17,7 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
+          <Route path="/" element={<Navigate to="/Inicio" />} />
           <Route path="/Reservas" element={<Reservas />} />
           <Route path="/Canchas" element={<Canchas />} />
           <Route path="/Inicio" element={<Home />} />
